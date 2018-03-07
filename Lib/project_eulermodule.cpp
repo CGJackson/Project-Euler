8f433df788fa83f887e990b062770ca4466780a1
Conor Jackson <conorgjackson@gmail.com>
2018-03-07 17:31:34 +0000

    Added primes_to_n to project_eulermodule.cpp

--- Lib/project_eulermodule.cpp
+++ Lib/project_eulermodule.cpp
@@ -4,7 +4,51 @@
 
 #include "Project_Euler.h"
 
-static PyObject *integer_sqrt(PyObject* self, PyObject *args){
+//Helper Functions
+
+// Creates a python list from a vector. The python objects in the list are
+// created from there Cpp counterparts by the function c_to_py_converter
+template<class T>
+static PyObject* PyList_FromVector(const std::vector<T> &vec,
+                                      PyObject* (*c_to_py_converter)(T)
+                                     ){
+    PyObject* list = Py_BuildValue("[]");
+
+    for(auto &e: vec){
+        PyObject *py_e = c_to_py_converter(e);
+        if(py_e == NULL){
+            Py_DECREF(list);
+            return NULL;
+        }
+
+        int failed_to_add = PyList_Append(list, py_e);
+        Py_DECREF(py_e);
+        if(failed_to_add){
+            Py_DECREF(list);
+            return NULL;
+        }
+    }
+    return list;
+}
+
+// C++ functions called by module methods
+
+static PyObject *primes_to_n(PyObject* self, PyObject* args){
+    Py_ssize_t n;
+    
+    if(!PyArg_ParseTuple(args, "n", &n)) return NULL;
+
+    if( n < 0 ){
+        PyErr_SetString(PyExc_ValueError, "Cannot find primes less than 0");
+        return NULL;
+    }
+
+    auto primes = project_euler::primes_to_n(n);
+
+    return PyList_FromVector(primes, PyLong_FromSsize_t);
+}
+
+static PyObject *integer_sqrt(PyObject* self, PyObject* args){
     long long n;
 
     if(!PyArg_ParseTuple(args, "L", &n)) return NULL;
@@ -29,8 +73,10 @@ static PyObject *integer_sqrt(PyObject* self, PyObject *args){
 // Sets up the python module 
 
 static PyMethodDef project_eulerMethods[] = {
-    {"integer_sqrt", integer_sqrt, METH_VARARGS, "Finds the integer square root of an integer or None is the number is not square"},
-    //{primes_to_n, primes_to_n, METH_VARARGS, returns a list of the prime numbers up to a given integer, n"}
+    {"integer_sqrt", integer_sqrt, METH_VARARGS,
+    "Finds the integer square root of an integer or None is the number is not square"},
+    {"primes_to_n", primes_to_n, METH_VARARGS,
+    "returns a list of the prime numbers up to a given integer, n"},
     {NULL, NULL, 0, NULL}
 };
 
