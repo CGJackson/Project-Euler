9bbb6457458186935aa859a5642ca998ef9715da
CGJackson <conorgjackson@gmail.com>
2018-03-07 18:07:50 +0000

    Added comments to Project_Euler.h
    Added comments documenting the behaviour of functions in Project_Euler.h. in particular explaining the behaviour of isqrt for unsigned types when the input is not a square number.

--- Lib/Project_Euler.h
+++ Lib/Project_Euler.h
@@ -8,6 +8,8 @@
 
 namespace project_euler {
 
+    // Returns a vecotr of the prime numbers up to n
+    
     std::vector<int> primes_to_n(const int n);
 
     std::vector<long int> primes_to_n(const long int n);
@@ -20,6 +22,16 @@ namespace project_euler {
 
     std::vector<long long unsigned> primes_to_n(const long long unsigned n);
 
+    // isqrt returns the integer square root of a number, or -1 if the number 
+    // is not square
+    //
+    // For unsigned types in the case of a non-sqare number, isqrt still 
+    // returns -1, whih is converted to the maximum size for that type
+    // due to the underflow behaviour of unsigned types. This is a safe
+    // return value as for x > 1, sqrt(x) < x < MAX_VAL, and MAX_VAL > 1
+    // so no number of a given type can have a sqrt of the largest value 
+    // for that type.
+    
     int isqrt(int n);
 
     long int isqrt(long int n);
@@ -27,7 +39,7 @@ namespace project_euler {
     long long int isqrt(long long int n);
 
     short int isqrt(short int n);
-
+    
     unsigned isqrt(unsigned n);
 
     unsigned long isqrt(unsigned long n);
