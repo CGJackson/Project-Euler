7d02b107f3951dbbf020b9cb38cc31610dfabb27
Conor Jackson <conorgjackson@gmail.com>
2018-07-13 21:40:17 +0100

    got solution to problem 152 to the point where it can be imported into ghci and the functions appear to give correct results, but performance is not good enough. Also no main function

--- Ongoing/Problem_152/writing_half_as_sum_of_inverse_squares.hs
+++ Ongoing/Problem_152/writing_half_as_sum_of_inverse_squares.hs
@@ -1,18 +1,19 @@
-inverseSquare :: Int -> Fractional
-inverseSquare n = 1/(n*n)
+inverseSquare :: Fractional a => Int -> a
+inverseSquare n = 1/(fromIntegral (n*n))
 
-sumInverseSquares [Int] -> Fractional
-sumInverseSquares = sum map inverseSquare 
+sumInverseSquares :: Fractional a => [Int] -> a
+sumInverseSquares = sum . map inverseSquare 
 
-getSumInverseSquares :: Fractional -> Int -> [[Int]]
+getSumInverseSquares :: (Fractional a , Ord a) => a -> Int -> [[Int]]
 getSumInverseSquares target limit = getSumInverseSquares' target (sumInverseSquares intRange) intRange
     where intRange = [2..limit]
-          getSumInverseSquares' :: Fractional -> Fractional -> [Int] -> [[Int]]
-          getSumInverseSquares' target remaining n:l
+          getSumInverseSquares' :: (Fractional a, Ord a) => a -> a -> [Int] -> [[Int]]
+          getSumInverseSquares' target remaining (n:l)
                 | target > remaining = []
                 | target == inverseSquare n = [n]:sumsWithoutN
                 | otherwise = (sumsWithN ++ sumsWithoutN)
                     where sumsWithN 
                                 | inverseSquare n > target = []
-                                | otherwise = map (n:) getSumInverseSquares' (target-inverseSquare n) (remaining-inverseSquare n) l
+                                | otherwise = map (n:) $ getSumInverseSquares' (target-inverseSquare n) (remaining-inverseSquare n) l
                           sumsWithoutN = getSumInverseSquares' target (remaining-inverseSquare n) l
+          getSumInverseSquares' _ _ [] = []
