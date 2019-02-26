8d8c8fb01b7e075068986ebdaf5a3dd166e4c252
Conor Jackson <conorgjackson@gmail.com>
2019-02-26 18:21:09 +0000

    tweeked PE header file

--- Lib/Project_Euler.h
+++ Lib/Project_Euler.h
@@ -24,17 +24,17 @@ namespace project_euler {
                                 }
     // Returns a vecotr of the prime numbers up to n
     
-    std::vector<int> primes_to_n(const int n);
+    std::vector<int> primes_to_n(int n);
 
-    std::vector<long int> primes_to_n(const long int n);
+    std::vector<long int> primes_to_n(long int n);
 
-    std::vector<long long int> primes_to_n(const long long int n);
+    std::vector<long long int> primes_to_n(long long int n);
 
-    std::vector<unsigned> primes_to_n(const unsigned n);
+    std::vector<unsigned> primes_to_n(unsigned n);
 
-    std::vector<long unsigned> primes_to_n(const long unsigned n);
+    std::vector<long unsigned> primes_to_n(long unsigned n);
 
-    std::vector<long long unsigned> primes_to_n(const long long unsigned n);
+    std::vector<long long unsigned> primes_to_n(long long unsigned n);
 
     int prime_counting_function(int);
 
